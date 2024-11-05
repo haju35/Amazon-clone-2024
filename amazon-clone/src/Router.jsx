8e51e790ac08a5912fe63e65e5f4bb92bd22dd94
haju35 <hajirasiraj217@gmail.com>
2024-11-05 11:38:09 -0800

    ProtectedRoute

--- amazon-clone/src/Router.jsx
+++ amazon-clone/src/Router.jsx
@@ -9,6 +9,7 @@ import Results from './Pages/Results/Results'
 import ProductDetail from './Pages/ProductDetail/ProductDetail'
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
+import ProtectedRoute from './Components/protectedRoute/protectedRoute'
 
 const stripePromise = loadStripe('pk_test_51QGQThDaICs9sTgPg8LtlhsjK9Njc6HoI5wwEgrM6cY2LG62Ddy0nm1ChSMWY5sL45lQjSgnW3OpS5XMxfUDOBvn00YaJo0f09');
 
@@ -22,10 +23,23 @@ function Routing() {
       <Route path='/' element={<LandingPage/>}/>
       <Route path='/auth' element={<Auth />}/>
       <Route path='/Payment'
-       element={<Elements stripe={stripePromise}>
+       element={
+        <ProtectedRoute
+        msg={"you must log in to pay"}
+        redirect={"/payment"}>
+          <Elements stripe={stripePromise}>
         <Payment/>
-        </Elements>}/>
-      <Route path='/Orders' element={<Orders/>}/>
+        </Elements>
+        </ProtectedRoute>
+       }/>
+      <Route path='/Orders' element={
+         <ProtectedRoute
+         msg={"you must log in to access orders"}
+         redirect={"/orders"}>
+           <Elements stripe={stripePromise}>
+           <Orders/>
+         </Elements>
+         </ProtectedRoute>}/>
       <Route path='/category/:categoryName' element={<Results/>}/>
       <Route path='/products/:productId' element={<ProductDetail/>}/>
       <Route path='/cart' element={<Cart/>}/>
